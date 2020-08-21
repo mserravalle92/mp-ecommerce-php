cdb3200f80b2a9e4d75cd08f9aaa56607694e009
Mariano <mserravalle92@gmail.com>
2020-08-21 01:24:07 -0300

    test

--- pending/index.php
+++ pending/index.php
@@ -1,5 +1,16 @@
 <h1>
     El pago se encuentra pendiente de aprobacion
 </h1>
+<hr>
+<table>
+    <tr> <th style="text-align:left"> Collection ID: </th> <td><?php echo $_GET['collection_id'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Collection Status: </th> <td><?php echo $_GET['collection_status'] ?></td> </tr>
+    <tr> <th style="text-align:left"> External Reference: </th> <td><?php echo $_GET['external_reference'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Payment Type: </th> <td><?php echo $_GET['payment_type'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Preference ID: </th> <td><?php echo $_GET['preference_id'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Site ID: </th> <td><?php echo $_GET['site_id'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Processing Mode: </th> <td><?php echo $_GET['processing_mode'] ?></td> </tr>
+    <tr> <th style="text-align:left"> Merchant Account ID: </th> <td><?php echo $_GET['merchant_account_id'] ?></td> </tr>
+</table>
 
 <a href="http://mserravalle-mp-ecommerce-php.herokuapp.com/">Volver al catalogo</a>
