52f4a80407fcd6f99ef7dab04f034eea29c7ccea
Mariano <mserravalle92@gmail.com>
2020-08-21 15:18:28 -0300

    webhook

--- webhook.php
+++ webhook.php
@@ -20,7 +20,7 @@ switch($_POST["type"]) {
         break;
 }
 
-file_put_contents('webhookResponse.json',json_encode($_REQUEST));
+file_put_contents('webhookResponse.json',json_encode($payment));
 
 ?>
 
