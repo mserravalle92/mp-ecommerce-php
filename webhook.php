5261678d21701318879039bf23d2965503800a97
Mariano <mserravalle92@gmail.com>
2020-08-21 15:30:10 -0300

    webhook

--- webhook.php
+++ webhook.php
@@ -5,24 +5,9 @@ require __DIR__ . '/env.php';
 
 MercadoPago\SDK::setAccessToken(ACCESS_TOKEN);
 
-$entityBody = file_get_contents('php://input');
+$webhookNotification = file_get_contents('php://input');
 
-switch($_POST["type"]) {
-    case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-        break;
-    case "plan":
-        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-        break;
-    case "subscription":
-        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-        break;
-    case "invoice":
-        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-        break;
-}
-
-file_put_contents('webhookResponse.json',$entityBody);
+file_put_contents('webhookResponse.json',$webhookNotification);
 
 ?>
 
